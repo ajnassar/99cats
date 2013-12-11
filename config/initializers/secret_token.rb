90af3afe5406faecb07408c5178d3403a9057c1e
= <=>
2013-12-11 13:25:24 -0800

    Create NinetyNineCats App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninetyninecats::Application.config.secret_token = 'e7955bd8350ccea81dd3b09b05dd2164258c03177e7b4905803cc85e7201c939494c685f0299321d0ba6c77cda2717c5bfa20c13df72e16c9387822f5a40da37'
